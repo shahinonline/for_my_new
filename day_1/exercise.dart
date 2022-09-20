b153cebce2e80ad080a2387f60c3cd1628f0113e
shahinonline <shahinonline@mail.ru>
2022-09-19 22:57:11 -0700

    19sep

--- day_1/exercise.dart
+++ day_1/exercise.dart
@@ -4,11 +4,33 @@ void main() {
   //You decided to want money from your father and you done . You have 6 dolors
   //l wanna know how much money do you have left?
 
+  double my_money = 2.5;
+  double bag_price = 5.5;
+  double needed_money = bag_price - my_money;
+  print("Lazim olan pul:$needed_money");
+  double now_my_money = 6;
+  double borrow = now_my_money - my_money;
+  print(borrow);
+  double res = now_my_money - bag_price;
+  print("Menim qalan pulum: $res-dir");
+
+  // 2.5+x=6
+
   // EXERCISE 2
   // There are 2 costumers and the name of costumers are Melek and Neymet.
   // They have 30 and 50 dolors
   // They wanna to buy the apple watch
   // cost of apple watch is 70 dolors
+
+  final String customer1 = "Melek";
+  final String customer2 = "Neymet";
+  int customer1_money = 30;
+  int customer2_money = 50;
+  int apple_watch = 70;
+  int all_money = customer1_money + customer2_money;
+  print(all_money);
+  int result = all_money - apple_watch;
+  print("Onlarin qalan pulu: $result-dir");
 
   // EXERCISE 3
   //There is a bank and the bank has 5 costumers, Ali,Melek,Neymet,Ehmet,Arif
