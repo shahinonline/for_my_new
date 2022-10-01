b57023a5b21a0c0f4cd70c637d65ec1d1606b71a
shahinonline <shahinonline@mail.ru>
2022-09-30 20:26:07 -0700

    switch

---
+++ day4/switch.dart
@@ -0,0 +1,23 @@
+void main() {
+  const int num1 = 1;
+  const int num2 = 2;
+  const int num3 = 3;
+  const int num4 = 4;
+  const int num5 = 5;
+  const int num6 = 6;
+  const int num7 = 7;
+  const int num8 = 8;
+  const int num9 = 9;
+  int myDreamNum = 6;
+
+  switch (myDreamNum) {
+    case num1:
+    case num2:
+    case num3:
+      print("Sizin xeyalinizdaki nomre ${myDreamNum}-dir");
+
+      break;
+    default:
+      print("Sizin xeyalinizdaki nomre ${myDreamNum}-dir");
+  }
+}
--- misallar.dart
+++ misallar.dart
@@ -45,4 +45,25 @@ void main(List<String> args) {
   } else {
     print("her kes futbol oynamalidir");
   }
+
+//EXERCISE 4
+// Bir bank var bu banka 2 müştəri gəlir adları və pulları ("mansur", 200 azn),("sahin", 400 azn)
+//Birdə bankın maximum məbləği var oda 3000 dir
+//Sahin bəy bankdan 500 manat kredit götürur.Sahin bəyin ən son plu nə qədər olur və bankın məbləgi nə qədər qalır.
+  // List<String> customers = [Mansur, Shahin];
+  // int bankMoney = 3000;
+  // print(customers[1]+=credit);
+  // int shahinsMoney = 400;
+  // int credit = 500;+
+
+//EXERCISE 5
+//Bir tələbə var onun 200 azn pulu var o ozunə paltar almaq istəyir amma mağazada onun alacaği paltarın qiyməti vurulmayıb
+//Əgər paltarın qiyməti 140 azn olsa o özunə birdənədə ayaqqabı alacaq olmasa sadəcə paltar ala biləcəy
+//EXERCISE 6
+//Bir adam özunə komputer alaq istəyir amma asus marka istəri əgər maqazində asus marka yoxdursa digər seçeney i acer olacaq
+//EXERCISE 7
+//Bir sitede bir şəxs qeydiyyatdan keçmək istəyir siteda qeydiyyat bölməsində bunda email və parol istəyir əgər istifadəçini parolu 8dən böyükdürse qeydiyyat olsun deyilsə parolu böyütməsini istəsin
+
+//EXERCISE 8
+//Bir futbol oyunu keçirilir oyunda əgər 3 qol vurularsa qolu vuran kamanda öz adamlarindan birini oyundan çixarmalidir əgər 9qol vurulubsa komandada nə qədər oyuncu qalldı
 }
