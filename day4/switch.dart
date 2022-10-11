ac9eac78c6b4f85ce5f27f1e6f64b2d4de7e4f49
shahinonline <shahinonline@mail.ru>
2022-10-10 19:56:05 -0700

    sha

--- day4/switch.dart
+++ day4/switch.dart
@@ -20,4 +20,32 @@ void main() {
     default:
       print("Sizin xeyalinizdaki nomre ${myDreamNum}-dir");
   }
+//Bir adam özunə komputer alaq istəyir amma asus marka istəri əgər maqazində asus marka yoxdursa digər seçeney i acer olacaq
+
+  const String myDream = "Asus";
+  const String otherOption = "Acer";
+
+  switch (myDream) {
+    case myDream:
+      print("");
+      break;
+    default:
+  }
+
+  //Bir tələbə var onun 200 azn pulu var o ozunə paltar almaq istəyir amma mağazada onun alacaği paltarın qiyməti vurulmayıb
+//Əgər paltarın qiyməti 140 azn olsa o özunə birdənədə ayaqqabı alacaq olmasa sadəcə paltar ala biləcəy
+
+  int studentMoney = 200;
+  int tshirtPrice = 200;
+  switch (tshirtPrice) {
+    case 140:
+    case 200:
+      print(
+          "Siz qalan pula yeni ${studentMoney - tshirtPrice}-a ayaqqabi da ala bilersiz");
+
+      break;
+    default:
+      print(
+          "Siz sadece Shirt ala bilersiz yeni sizin qaliq pulunuz ${studentMoney - tshirtPrice}-dir");
+  }
 }
--- misallar.dart
+++ misallar.dart
@@ -66,4 +66,99 @@ void main(List<String> args) {
 
 //EXERCISE 8
 //Bir futbol oyunu keçirilir oyunda əgər 3 qol vurularsa qolu vuran kamanda öz adamlarindan birini oyundan çixarmalidir əgər 9qol vurulubsa komandada nə qədər oyuncu qalldı
+
+  int redTeam = 11;
+  int blueTeam = 11;
+  int lineGoal = 3;
+  int goalCount = 4;
+  if (goalCount >= lineGoal) {
+    print("qirmizi komandada ${redTeam - 1} sayda futbolcu qaldi");
+  }
+//EXERCISE
+// Bir qadinin pulu 'Mansur' adli insanin adinin uzluquna beraberdir
+// Qadinin puluna adi 'Sahin' olan adamin adinin uzunluqu qeder pul eleva olunur
+// Qadinin son pulu nedir?
+
+  String name2 = "Shahin";
+  String name = "Mansur";
+  int womansMoney = name.length;
+  int res = womansMoney + name2.length;
+  print(res);
+//EXERCISE
+//Senin icinde adalar olan bir cedvelin var ve adlar "mansur",'sahin','tahir','elgun','telman','mahir','xeyyam' dir
+//Men ekranda senin cedvelindeki 4uncu sirada olan adamin adini gormek isteyirem
+
+  List<String> namesList = [
+    "Mansur",
+    "Shahin",
+    "Tahir",
+    "Elgun",
+    "Telman",
+    "Mahir",
+    "Xeyyam"
+  ];
+  print(namesList[3]);
+//EXERCISE
+//Senin icinde adalar olan bir cedvelin var ve adlar "mansur",'sahin','tahir','elgun','telman','mahir','xeyyam' dir
+//Eger senin listindeki ilk ad mahirdirse ekranda Mahir xos gelmisen yazilsin
+//Eger senin listinin uzunluqu 6dan boyukdurse senin listine yeni bir adam elave olunacaq ve onun adi 'nicat' dir
+
+  List<String> nameList = [
+    "Mansur",
+    "Shahin",
+    "Tahir",
+    "Elgun",
+    "Telman",
+    "Mahir",
+    "Xeyyam"
+  ];
+  if (nameList[0] == "Mahir") {
+    print("Xosh Gelmisiz");
+  } else {
+    print("ilk ad mahir deyildir");
+  }
+  if (nameList.length >= 6) {
+    nameList.insert(0, "Nicat");
+    print(nameList);
+  } else {}
+//EXERCISE
+//Bir adamin 5000 manat pulu var
+//adamin 6 illik xerclerini yazdiqi bir listi var ve onun listindeki reqemler 800,1500,700,900,3000,2500
+//Eger bu adam ne vaxtsa 900 manat pul xecleyibse indiki pulundan hemin 900 manati cixin
+
+  int manMoney = 5000;
+  List<int> count = [800, 1500, 700, 900, 3000, 2500];
+  switch (count[3]) {
+    case 900:
+      int res = manMoney - count[3];
+      print(res);
+      break;
+    default:
+  }
+//EXERCISE
+//adamin 6 illik xerclerini yazdiqi bir listi var ve onun listindeki reqemler 800,1500,700,900,3000,2500
+//Ekranda adamin 5 il erzinde xerclediyi pulu gormek isteyirem
+//Eger bu adam 5 il erzinde 6000 manatdan cox pul xercleyibse ona sen pulun qedirinin bilmirsen adli melumat ver
+  List<int> manSpending = [800, 1500, 700, 900, 3000, 2500];
+  int result = manSpending[0] +
+      manSpending[1] +
+      manSpending[2] +
+      manSpending[3] +
+      manSpending[4];
+  print(result);
+//EXERCISE
+//Bir telebe imtahandan 79 bal alir
+//Eger telebe imtahandan 51 den cox 61den az yigsa F,61den cox 71den az yigsa D, 71 den cox 81 den az yigsa C,81 den cox 91den az yigsa B,91den cox yigsa A alacaq
+//Telebenin Herifi qiymeti nedir
+
+  int studentScore = 79;
+  if (studentScore > 51 && studentScore < 61) {
+    print("F");
+  } else if (studentScore > 61 && studentScore < 71)
+  else {}
+
+//EXERCISE
+//Bir maqazin gunluk olaraq 600 manat alver edir
+//Vergi sistemine esasende eger bir is yeri gunluk min manatdan cox alver etse ayliq 300 vergi odeyir amma 1000 den az olsa 200 manat odeyir
+//Bu maqazin ne qeder vergi odeyir
 }
